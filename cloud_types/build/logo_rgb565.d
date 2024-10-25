6718ce0ccfb0ba4f56fce02162f929defdb75441
LJ-Zer <lj.dev02@gmail.com>
2024-10-25 14:22:49 +0800

    Add files via upload

---
+++ cloud_types/build/logo_rgb565.d
@@ -0,0 +1,2 @@
+C:/MaximSDK/Examples/MAX78000/CNN/cloud_types/build/logo_rgb565.o: \
+ TFT/fthr/logo_rgb565.c
---
+++ cloud_types/sampleoutput.h
@@ -0,0 +1,5 @@
+// This file was @generated automatically
+
+#define SAMPLE_OUTPUT { \
+  0x50401000, 0xffffffff, 0x00000004, 0xfffff7c4, 0xfffffea9, 0x000009c5, 0x00000365, 0x00000000 \
+}
